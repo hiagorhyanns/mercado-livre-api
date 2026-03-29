64a0b970b909f04b63c29dcadba85ff50aecd4ab
hiagorhyanns <109935872+hiagorhyanns@users.noreply.github.com>
2026-03-28 22:34:18 -0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,13 @@
 export default function handler(req, res) {
-  const url = 'https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}';
+  const client_id = process.env.CLIENT_ID;
+  const redirect_uri = process.env.REDIRECT_URI;
+
+  const url =
+    "https://auth.mercadolivre.com.br/authorization" +
+    "?response_type=code" +
+    "&client_id=" + client_id +
+    "&redirect_uri=" + redirect_uri;
+
   res.writeHead(302, { Location: url });
   res.end();
 }
